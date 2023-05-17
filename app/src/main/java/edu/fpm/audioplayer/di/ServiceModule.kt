78c95f60e76c03b82ad34a8f61f0538d84a40428
Yaroslav Khodos <khodos.jar@gmail.com>
2023-05-17 17:37:07 +0300

    implemented MusicService

--- app/src/main/java/edu/fpm/audioplayer/di/ServiceModule.kt
+++ app/src/main/java/edu/fpm/audioplayer/di/ServiceModule.kt
@@ -3,12 +3,8 @@ package edu.fpm.audioplayer.di
 import android.content.Context
 import com.google.android.exoplayer2.C
 import com.google.android.exoplayer2.ExoPlayer
-import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.android.exoplayer2.audio.AudioAttributes
-import com.google.android.exoplayer2.upstream.DataSource
 import com.google.android.exoplayer2.upstream.DefaultDataSource
-import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
-import com.google.android.exoplayer2.util.Util
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -41,6 +37,5 @@ object ServiceModule {
     @Provides
     fun provideDataSourceFactory(
         @ApplicationContext context: Context
-    ) = DefaultDataSource.Factory(context)
-    //= DefaultDataSourceFactory(context, Util.getUserAgent(context, "AudioPlayer App"))
+    ) = DefaultDataSource.Factory(context).createDataSource()
 }
